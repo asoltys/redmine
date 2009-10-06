48cf1e8be3caf303bf3f76b23f693dc4fb4290d6
Adam Soltys <asoltys@gmail.com>
2009-10-05 21:17:53 -0700

    Cleaned up git test repository

--- test/functional/repositories_git_controller_test.rb
+++ test/functional/repositories_git_controller_test.rb
@@ -52,24 +52,24 @@ class RepositoriesGitControllerTest < ActionController::TestCase
       assert_not_nil assigns(:entries)
       assert_equal 8, assigns(:entries).size
       assert assigns(:entries).detect {|e| e.name == 'images' && e.kind == 'dir'}
+      assert assigns(:entries).detect {|e| e.name == 'module' && e.kind == 'link'}
       assert assigns(:entries).detect {|e| e.name == 'sources' && e.kind == 'dir'}
+      assert assigns(:entries).detect {|e| e.name == '.gitmodules' && e.kind == 'file'}
       assert assigns(:entries).detect {|e| e.name == 'README' && e.kind == 'file'}
       assert assigns(:entries).detect {|e| e.name == 'copied_README' && e.kind == 'file'}
       assert assigns(:entries).detect {|e| e.name == 'new_file.txt' && e.kind == 'file'}
       assert assigns(:entries).detect {|e| e.name == 'renamed_test.txt' && e.kind == 'file'}
-      assert assigns(:entries).detect {|e| e.name == '.gitmodules' && e.kind == 'file'}
-      assert assigns(:entries).detect {|e| e.name == 'submodule' && e.kind == 'link'}
 
       assert_tag :tag => 'a', :content => /images/, :attributes => {
         :class => /icon-folder/
       }
 
-      assert_tag :tag => 'a', :content => /README/, :attributes => {
-        :class => /icon-file/
+      assert_tag :tag => 'a', :content => /module/, :attributes => {
+        :class => /icon-link/
       }
 
-      assert_tag :tag => 'a', :content => /submodule/, :attributes => {
-        :class => /icon-link/
+      assert_tag :tag => 'a', :content => /README/, :attributes => {
+        :class => /icon-file/
       }
     end
 
