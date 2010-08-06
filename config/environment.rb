2797241954726d23627150118f2aa72a1e9b8a5b
Patrick Naubert <patrickn@xelerance.com>
2010-08-06 15:00:18 -0400

    We depend on Postgresql

--- config/environment.rb
+++ config/environment.rb
@@ -58,4 +58,6 @@ Rails::Initializer.run do |config|
   if File.exists?(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
     instance_eval File.read(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
   end
+
+  config.gem "postgres"
 end
