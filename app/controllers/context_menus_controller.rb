6abf580be1a6de966a53d8d48b514fdfb33dae71
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-25 20:31:18 -0600

    setting up menu variables more betterly

--- app/controllers/context_menus_controller.rb
+++ app/controllers/context_menus_controller.rb
@@ -42,8 +42,13 @@ class ContextMenusController < ApplicationController
   end
 
   def time_entries
-    @time_entries = TimeEntry.find(:all, :conditions => {:project_id => 2 })
-    @activities = Enumeration.find(:all)
+    @time_entries = TimeEntry.all(:conditions => {:id => params[:ids]}, :include => :project)
+    
+    @projects = @time_entries.collect(&:project).compact.uniq
+    @project = @projects.first if @projects.size == 1
+    
+    @activities = TimeEntryActivity.shared.active
+
     @can = {:edit => User.current.allowed_to?(:log_time, @projects),
             :update => User.current.allowed_to?(:log_time, @projects),
             :delete => User.current.allowed_to?(:log_time, @projects)
